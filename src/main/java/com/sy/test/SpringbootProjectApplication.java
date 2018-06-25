91b657f7d8b289119e7a8056d25e8861a6e03e8f
shenyang <shenyang@seassoon.com>
2018-06-25 17:20:52 +0800

    first commit

---
+++ src/main/java/com/sy/test/SpringbootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.sy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+	}
+}
